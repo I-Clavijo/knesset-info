7924a2a8d6233e6a369e22caf9d8b85b799aebce
Ivan Clavijo <ivan.clavijo2@gmail.com>
2025-02-04 10:06:34 +0200

    fixed ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Hero from "../components/Hero";
 import BillCard from "../components/BillCard";
+import Footer from "../containers/Footer";
 
 const bill1 = {
   id: 1,
   name: "חוק הגיוס",
   amount: 120,
-}
+};
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Hero />
-        <BillCard {...bill1}/>
+        <BillCard {...bill1} />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+
+      <Footer />
     </div>
   );
 }
--- containers/Navbar.tsx
+++ containers/Navbar.tsx
@@ -4,18 +4,19 @@ import { Navbar } from "flowbite-react";
 
 export default function NavbarContainer() {
   return (
-    <Navbar fluid rounded dir="ltr">
+    <Navbar fluid rounded className="rounded-none">
       <Navbar.Brand href="#">
-        <span className="self-center whitespace-nowrap text-2xl font-semibold text-blue-500 dark:text-blue-500">
-          info
-        </span>
-        &nbsp;
         <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
           כנסת
+        </span>
+        &nbsp;
+        <span className="self-center whitespace-nowrap text-2xl font-semibold text-blue-500 dark:text-blue-500">
+          info
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
+        <p></p>
         <Navbar.Link href="#">בית</Navbar.Link>
         <Navbar.Link href="#">אודות</Navbar.Link>
         <Navbar.Link href="#">הצטרף</Navbar.Link>
