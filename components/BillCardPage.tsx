a99b69c2b949b76624a0d882983dfd6e70ed3df9
Ivan Clavijo <ivan.clavijo2@gmail.com>
2025-02-14 00:41:02 +0200

    ui improvements

--- components/BillCardPage.tsx
+++ components/BillCardPage.tsx
@@ -16,91 +16,115 @@ const BillCardPage = ({
   Initiators = [],
   Category = "ללא קטגוריה",
   FilePath = "",
-}: //VotesUp = 0,
-//VotesDown = 0,
-//Comments = 0,
-Bill) => {
-  console.log(Initiators);
+}: Bill) => {
   return (
-    <div className="flex flex-wrap gap-4 min-h-screen">
+    <div className="flex flex-wrap gap-4 min-h-screen p-4">
+      {/* Bill Details Card */}
       <Card className="max-w-lg">
-        <div className="flex flex-col h-full">
-          <span className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">
+        <div className="flex flex-col h-full space-y-4">
+          <h1 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             {Name}
-          </span>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
+          </h1>
+
+          <p className="text-gray-700 dark:text-gray-400">
+            עדכון אחרון:{" "}
             {new Date(LastUpdatedDate).toLocaleDateString("en-GB", {
               day: "2-digit",
               month: "2-digit",
               year: "numeric",
             })}
           </p>
-          <hr className="my-2 border-gray-300 dark:border-gray-600" />
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {Summary}
-          </p>
-          <p className="font-normal text-gray-700 dark:text-white inline-block">
-            שינויים:
-          </p>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {MainInstructions}
-          </p>
-          <p className="font-normal text-gray-700 dark:text-white inline-block">
-            השפעות:
-          </p>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {Impacts}
-          </p>
-          <hr className="my-2 border-gray-300 dark:border-gray-600" />
-          <p className="font-normal text-gray-700 dark:text-white inline-block">
-            קטגוריה:{" "}
-            <span className="text-gray-700 dark:text-gray-400">
-              {myCategories.find((category) => category.id === Number(Category))
-                ?.name || Category}
-            </span>
-          </p>
-          <div className="mt-3">
-            <Button onClick={() => (window.location.href = FilePath)}>
+
+          <hr className="border-gray-300 dark:border-gray-600" />
+
+          <section>
+            <h2 className="font-semibold text-gray-700 dark:text-white">
+              תקציר:
+            </h2>
+            <p className="text-gray-700 dark:text-gray-400">{Summary}</p>
+          </section>
+
+          <section>
+            <h2 className="font-semibold text-gray-700 dark:text-white">
+              שינויים:
+            </h2>
+            <p className="text-gray-700 dark:text-gray-400">
+              {MainInstructions}
+            </p>
+          </section>
+
+          <section>
+            <h2 className="font-semibold text-gray-700 dark:text-white">
+              השפעות:
+            </h2>
+            <p className="text-gray-700 dark:text-gray-400">{Impacts}</p>
+          </section>
+
+          <hr className="border-gray-300 dark:border-gray-600" />
+
+          <section>
+            <h2 className="font-semibold text-gray-700 dark:text-white">
+              קטגוריה:{" "}
+              <span className="font-normal text-gray-700 dark:text-gray-400">
+                {myCategories.find(
+                  (category) => category.id === Number(Category)
+                )?.name || Category}
+              </span>
+            </h2>
+          </section>
+
+          <div className="mt-auto">
+            <Button
+              onClick={() => (window.location.href = FilePath)}
+              className="w-full"
+            >
               להורדת הצעת החוק
             </Button>
           </div>
         </div>
       </Card>
 
-      <Card className=" grow max-w-lg">
-        <div className="flex flex-col h-full">
-          <div className="flex items-center justify-center bg-gray-100 dark:bg-gray-700 p-2 rounded-lg">
-            <span className="text-center text-md tracking-tight text-gray-900 dark:text-white my-auto">
+      {/* Initiators Card */}
+      <Card className="grow max-w-lg">
+        <div className="flex flex-col h-full space-y-4">
+          <header className="bg-gray-100 dark:bg-gray-700 p-3 rounded-lg text-center">
+            <h2 className="text-md font-semibold text-gray-900 dark:text-white">
               יזמים
-            </span>
-          </div>
-          <div className={`grid grid-cols-3 gap-4 p-4`}>
-            {Initiators.length > 0 &&
-              Initiators.map((initiator: number) => (
-                <div key={initiator} className="flex flex-col items-center">
-                  <Image
-                    src={`/images/knessetMembers/${initiator}.jpeg`}
-                    alt="bill"
-                    width={100}
-                    height={100}
-                    className="rounded-lg"
-                  />
-                  <div className="text-center">
-                    <span>
-                      {
-                        members.find((member) => member.PersonID === initiator)
-                          ?.FirstName
+            </h2>
+          </header>
+
+          <div className="grid grid-cols-3 gap-4 p-4">
+            {Initiators.length > 0 ? (
+              Initiators.map((initiator: number) => {
+                const member = members.find(
+                  (member) => member.PersonID === initiator
+                );
+                return (
+                  <div key={initiator} className="flex flex-col items-center">
+                    <Image
+                      src={`/images/knessetMembers/${initiator}.jpeg`}
+                      alt={
+                        member
+                          ? `${member.FirstName} ${member.LastName}`
+                          : "יזם"
                       }
-                    </span>{" "}
-                    <span>
-                      {
-                        members.find((member) => member.PersonID === initiator)
-                          ?.LastName
-                      }
-                    </span>
+                      width={100}
+                      height={100}
+                      className="rounded-lg"
+                    />
+                    <div className="text-center mt-2">
+                      <span className="text-gray-700 dark:text-gray-400">
+                        {member?.FirstName} {member?.LastName}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })
+            ) : (
+              <p className="text-gray-700 dark:text-gray-400 col-span-3 text-center">
+                אין יזמים זמינים
+              </p>
+            )}
           </div>
         </div>
       </Card>
--- components/Ranking.tsx
+++ components/Ranking.tsx
@@ -1,16 +1,63 @@
 "use client";
 
 import { Card } from "flowbite-react";
-import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import members from "@/app/members"; // Assuming this now includes NumberOfBills
 
 const Ranking = () => {
+  const Initiators = [427, 427, 427]; // These should now be PersonIDs
+  const numberOfBills = 3;
+
   return (
-    <Link href={"/"} legacyBehavior>
-      <Card className="w-full h-full flex items-center justify-center">
-        <div className="text-center">Ranking Component Content</div>
-      </Card>
-    </Link>
+    <Card
+      className={`w-full h-full flex flex-col bg-gradient-to-r from-sky-700 to-indigo-800 shadow-lg hover:shadow-xl transition-shadow duration-300`}
+    >
+      <div className="flex flex-col h-full">
+        <div className="flex items-center justify-center ">
+          <span className="text-center text-xl font-bold tracking-tight text-white">
+            חברי כנסת שיזמו יותר הצעות חוק
+          </span>
+        </div>
+        <div className={`grid grid-cols-3 gap-8 p-6`}>
+          {Initiators.length > 0 &&
+            Initiators.map((initiator: number, index: number) => {
+              const memberData = members.find(
+                (member) => member.PersonID === initiator
+              );
+              if (!memberData) return null;
+
+              return (
+                <div
+                  key={initiator}
+                  className="flex flex-col items-center transform hover:scale-105 transition-transform duration-300"
+                >
+                  <div className="relative group">
+                    <Image
+                      src={`/images/knessetMembers/${initiator}.jpeg`}
+                      alt="bill"
+                      width={80}
+                      height={80}
+                      className="rounded-lg shadow-md group-hover:shadow-lg transition-shadow duration-300"
+                    />
+                    <div className="absolute top-0 right-0 bg-gray-900 text-white rounded-full w-7 h-7 flex items-center justify-center transform -translate-y-2 translate-x-2">
+                      {index + 1}
+                    </div>
+                  </div>
+                  <div className="text-center mt-2">
+                    <span className="text-sm font-semibold text-white">
+                      {memberData.FirstName} {memberData.LastName}
+                    </span>
+                    <div className="text-sm text-gray-300 ">
+                      {numberOfBills} הצעות חוק
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </Card>
   );
 };
 
